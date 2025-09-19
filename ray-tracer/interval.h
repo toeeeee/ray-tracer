f448d7cae49f2f960e596a5c4152d0c74e9a43bb
toeeeee <anthonyanthonyanthonynguyen@gmail.com>
2025-09-19 13:20:05 -0700

    refactoring of names to make it more inline with book and fixed the hittablelist errors

---
+++ ray-tracer/interval.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#include "rtweekend.h"
+
+class interval {
+public:
+	double min, max;
+
+	interval() : min(+infinity), max(-infinity) {}
+
+	interval(double min, double max) : min(min), max(max) {}
+
+	double size() const { return max - min;  }
+
+	bool contains(double x) { return min <= x && x <= max; }
+
+	bool surrounds(double x) { return min < x && x < max; }
+
+	static const interval empty, universe;
+};
+
+const interval interval::empty = interval();
+const interval interval::universe = interval(-infinity, +infinity);
+
+#endif
